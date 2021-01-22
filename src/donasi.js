8e41e23425ea6ba93cb5c2489a0f52067c33b048
cychrisfds <77716224+cychrisfds@users.noreply.github.com>
2021-01-22 04:13:52 -0300

    Add files via upload

---
+++ src/donasi.js
@@ -0,0 +1,22 @@
+﻿const donasi = (prefix) => {
+	return `╔════════════════════
+║ *MENU DE DOAÇAO*
+╠════════════════════
+║╭──❉ *nao precisa doarKKKK* ❉─────
+║│➸ *Cychris*: wa.me/5581996207906 
+║│➸ *grupo do zap*: https://chat.whatsapp.com/JzMIJnQwajrEbWUf25kReM 
+║│➸ *grupo do telegram*: https://t.me/shitpostadoKKKKKKK 
+║│➸ *grupo do discord*: https://discord.com/invite/fDkrq6546Y 
+║╰──────────────────
+╠════════════════════
+║         06-11-2002
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║         RAMLAN
+╠════════════════════
+║ _*POWERED BY CYCHRIS*_
+╚════════════════════
+`
+}
+
+exports.donasi = donasi
